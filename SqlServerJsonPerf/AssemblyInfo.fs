154cbc43ad2efad0db9eadc3b9cc95c29c96ade1
robertmattingly <rmattingly@ruggedsoftware.net>
2023-12-07 09:12:14 -0500

    UserSecrets setup

---
+++ SqlServerJsonPerf/AssemblyInfo.fs
@@ -0,0 +1,7 @@
+module SqlServerJsonPerf.AssemblyInfo
+
+open Microsoft.Extensions.Configuration.UserSecrets
+
+[<UserSecretsId("8838c559-9dda-4827-be58-dfbd7ba45d8a")>]
+
+do()
--- SqlServerJsonPerf/Program.fs
+++ SqlServerJsonPerf/Program.fs
@@ -1,2 +1,22 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+
+namespace SqlServerJsonPerf
+
+open Microsoft.Extensions.Configuration
+
+type Program() =
+    do()
+
+module Program =
+    [<EntryPoint>]
+    let main argv =
+        let config = ConfigurationBuilder().AddUserSecrets<Program>().Build()
+        
+        let username = config.["Sql:Username"]
+        let password = config.["Sql:Password"]
+        
+        printfn $"Running as {username}"
+        
+        printfn "Hello from F#"
+        
+        0
